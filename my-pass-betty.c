7ff78721330da59a5e8844104ada02e4ed991372
chinedunweke618@gmail.com <ChineduSE>
2023-04-26 13:15:10 -0700

    hard shell

---
+++ my-pass-betty.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * factorial - Calculates the factorial of a given number.
+ * @n: The number to calculate the factorial for.
+ *
+ * Return: The factorial of n
+ */
+unsigned int factorial(unsigned int n)
+{
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	unsigned int num = 5;
+	unsigned int result;
+
+	result = factorial(num);
+
+	printf("The factorial of %u is: %u\n", num, result);
+
+	return (0);
+}
